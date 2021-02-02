59086f13dc426afae491747b7c81e67e52a80925
Mario <marioquartz@gmail.com>
2021-02-02 09:41:46 +0100

    Batch of five Regions

---
+++ src/Provider/Spain/Asturias.php
@@ -0,0 +1,46 @@
+<?php
+
+/*
+ * This file is part of the umulmrum/holiday package.
+ *
+ * (c) Stefan Kruppa
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Umulmrum\Holiday\Provider\Spain;
+
+use Umulmrum\Holiday\Constant\HolidayName;
+use Umulmrum\Holiday\Constant\HolidayType;
+use Umulmrum\Holiday\Model\Holiday;
+use Umulmrum\Holiday\Model\HolidayList;
+use Umulmrum\Holiday\Provider\CommonHolidaysTrait;
+use Umulmrum\Holiday\Provider\HolidayProviderInterface;
+use Umulmrum\Holiday\Provider\Religion\ChristianHolidaysTrait;
+
+class Asturias extends Spain
+{
+    use ChristianHolidaysTrait;
+    use CommonHolidaysTrait;
+    use CompensatoryDaysTrait;
+
+    /**
+     * {@inheritdoc}
+     */
+    public function calculateHolidaysForYear(int $year): HolidayList
+    {
+        $holidays = parent::calculateHolidaysForYear($year);
+        $holidays->add($this->getMaundyThursday($year, HolidayType::OFFICIAL | HolidayType::DAY_OFF));
+        $holidays->add($this->getRegionalDay($year));
+        $this->addCompensatoryConstitutionDay($holidays, $year);
+        $this->addCompensatoryAllSaints($holidays, $year);
+
+        return $holidays;
+    }
+
+    private function getRegionalDay(int $year, int $additionalType = HolidayType::OTHER): Holiday
+    {
+        return Holiday::create(HolidayName::REGIONAL_DAY, "{$year}-09-09", HolidayType::OFFICIAL | HolidayType::DAY_OFF | $additionalType);
+    }
+}
